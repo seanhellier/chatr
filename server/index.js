71e77f4f5bdff0a0e0758b49d7e3cce6fa974df2
Sean Leif Hellier <sean@newtropolis.com>
2020-09-07 17:59:08 -0400

    added join and chat components

--- server/index.js
+++ server/index.js
@@ -2,10 +2,22 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 
+const PORT = process.env.PORT || 5000;
+
+const router = require("./router");
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-const PORT = process.env.PORT || 5000;
+io.on(`conneciton`, (socket) => {
+	console.log("we have a new connection");
+
+	socket.on(`disconnect`, () => {
+		console.log("User left");
+	});
+});
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`server has started on port ${PORT}`));
