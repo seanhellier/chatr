b3e09981e4b920e04c0557a9932c0e27757b469a
Sean Leif Hellier <sean@newtropolis.com>
2020-09-07 20:26:01 -0400

    added admin messaging

--- server/index.js
+++ server/index.js
@@ -13,10 +13,23 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on(`connection`, (socket) => {
-	console.log("we have a new connection");
-
 	socket.on(`join`, ({ name, room }, callback) => {
-		console.log(name, room);
+		const { error, user } = addUser({ id: socket.id, name, room });
+
+		if (error) return callback(error);
+
+		socket.emit("message", {
+			user: "admin",
+			text: `${user.name}, welcome to room ${user.room}.`,
+		});
+
+		socket.broadcast
+			.to(user.room)
+			.emit("message", { user: "admin", text: `${user.name} has joined!` });
+
+		socket.join(user.room);
+
+		callback();
 	});
 
 	socket.on(`disconnect`, () => {
