0f76fe97c25f98255ff104a1094320dac8a5fceb
Sean Leif Hellier <sean@newtropolis.com>
2020-09-08 12:30:42 -0400

    added message component and styling

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -3,6 +3,7 @@ import queryString from "query-string";
 import io from "socket.io-client";
 
 import "./Chat.css";
+import InfoBar from "../InfoBar/InfoBar";
 
 let socket;
 
@@ -49,6 +50,7 @@ const Chat = ({ location }) => {
 	return (
 		<div className="outerContainer">
 			<div className="container">
+				<InfoBar room={room} />
 				<input
 					value={message}
 					onChange={(event) => setMessage(event.target.value)}
