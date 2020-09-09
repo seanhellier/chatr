46a5e6bd6dac59e660c6ee252d0e0c173238124b
Sean Leif Hellier <sean@newtropolis.com>
2020-09-09 16:40:41 -0400

    added chat room list and styling

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,42 +2,47 @@ import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
 
-import "./Chat.css";
+import TextContainer from "../textContainer/textContainer";
+import Messages from "../Messages/Messages";
 import InfoBar from "../InfoBar/InfoBar";
 import Input from "../Input/Input";
-import Messages from "../Messages/Messages";
+
+import "./Chat.css";
 
 let socket;
 
 const Chat = ({ location }) => {
 	const [name, setName] = useState("");
 	const [room, setRoom] = useState("");
+	const [users, setUsers] = useState("");
 	const [message, setMessage] = useState("");
 	const [messages, setMessages] = useState([]);
-	const ENDPOINT = "localhost:5000";
+	const ENDPOINT = "https://project-chat-application.herokuapp.com/";
 
 	useEffect(() => {
 		const { name, room } = queryString.parse(location.search);
 
 		socket = io(ENDPOINT);
 
-		setName(name);
 		setRoom(room);
+		setName(name);
 
-		socket.emit(`join`, { name, room }, () => {});
-
-		return () => {
-			socket.emit("disconnect");
-
-			socket.off();
-		};
+		socket.emit("join", { name, room }, (error) => {
+			if (error) {
+				alert(error);
+			}
+		});
 	}, [ENDPOINT, location.search]);
 
 	useEffect(() => {
 		socket.on("message", (message) => {
-			setMessages([...messages, message]);
+			setMessages((messages) => [...messages, message]);
 		});
-	}, [messages]);
+
+		socket.on("roomData", ({ users }) => {
+			setUsers(users);
+		});
+	}, []);
 
 	const sendMessage = (event) => {
 		event.preventDefault();
@@ -46,8 +51,6 @@ const Chat = ({ location }) => {
 			socket.emit("sendMessage", message, () => setMessage(""));
 		}
 	};
-
-	console.log(message, messages);
 
 	return (
 		<div className="outerContainer">
@@ -60,6 +63,7 @@ const Chat = ({ location }) => {
 					sendMessage={sendMessage}
 				/>
 			</div>
+			<TextContainer users={users} />
 		</div>
 	);
 };
